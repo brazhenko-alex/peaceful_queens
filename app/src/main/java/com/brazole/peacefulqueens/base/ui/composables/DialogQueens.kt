83e611e004ed96fd5518dc48e760b54963cf83d4
“Olexandr1904” <Brazhenko.p@gmail.com>
2025-12-14 23:46:01 +0200

    Refactor DialogQueens to replace Spacer with SpacerInBlock for consistent spacing

--- app/src/main/java/com/brazole/peacefulqueens/base/ui/composables/DialogQueens.kt
+++ app/src/main/java/com/brazole/peacefulqueens/base/ui/composables/DialogQueens.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -52,14 +50,14 @@ fun DialogQueens(
                         color = AppTheme.color.winGreen,
                         textAlign = TextAlign.Center
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
+                    SpacerInBlock()
                 }
                 Text(
                     text = message,
                     style = AppTheme.typography.textMedium,
                     textAlign = TextAlign.Center
                 )
-                Spacer(modifier = Modifier.height(16.dp))
+                SpacerInBlock()
                 Row(
                     horizontalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
